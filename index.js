98b6f41223272306dfeb2c78280846b42eeabfca
Emmanuel <emmanueldonkor8989@gmail.com>
2025-07-24 09:27:02 +0000

    cron.js added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/authRoutes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import job from "./lib/cron.js";
 
 
 
@@ -11,6 +12,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+job.start();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser())
